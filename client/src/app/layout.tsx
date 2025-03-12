47de68602e3d81e11e9f904dce7ce6186d01586e
AlexanderKioko <muuoalex136@gmail.com>
2025-03-12 06:17:41 +0300

    Fixed layout.tsx error by converting to a Client Component

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,19 +1,22 @@
+"use client"; // Convert to Client Component
+
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "@/context/AuthContext";
+import { ReactNode } from "react";
 
 // Creates a QueryClient instance
 const queryClient = new QueryClient();
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
-        {/* Navbar is always visible */}
+        {/* Navbar is included globally */}
         <Navbar />
 
-        {/* Wraps the entire app with QueryClientProvider & AuthProvider */}
+        {/* Wrapping app with QueryClientProvider & AuthProvider */}
         <QueryClientProvider client={queryClient}>
           <AuthProvider>
             {children}
